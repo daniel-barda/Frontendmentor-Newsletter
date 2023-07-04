c0239abe95d97925ce007d676d18d2d1268e4b17
Daniel Barda <84409940+daniel-barda@users.noreply.github.com>
2023-07-04 16:29:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector(".content__form");
+const containerEL = document.querySelector(".container");
+const successEL = document.querySelector(".success");
+const btn = document.querySelector(".btn--success");
+console.log(successEL);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  containerEL.classList.add("hide");
+  successEL.classList.remove("hide");
+  form.childNodes[3].value = "";
+});
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  containerEL.classList.remove("hide");
+  successEL.classList.add("hide");
+});
